46728f8d1d9af4cc249794b57849c919b1a77af9
Jonas Primbs <jonas.primbs@uni-tuebingen.de>
2025-09-12 14:29:13 +0200

    adapt to flutter_blue_plus 1.35

--- lib/src/windows/windows.dart
+++ lib/src/windows/windows.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
 
+import 'package:flutter_blue_plus_platform_interface/flutter_blue_plus_platform_interface.dart';
 import 'package:flutter_blue_plus_windows/flutter_blue_plus_windows.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart' as FBP;
 import 'package:path/path.dart' as path;
